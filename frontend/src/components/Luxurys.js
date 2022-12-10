bf0c78ea77a52ad16d9080e8aa10ecee17fc285a
JasonIvan <95385642+JasonIvan@users.noreply.github.com>
2022-12-10 00:40:58 -0500

    Add files via upload

---
+++ frontend/src/components/Luxurys.js
@@ -0,0 +1,70 @@
+import React from "react";
+import luxury from "./RightCar4u-pic/luxury.jpeg";
+import luxury2 from "./RightCar4u-pic/luxury2.jpeg";
+import luxury3 from "./RightCar4u-pic/luxury3.jpeg";
+import Carousel from 'react-bootstrap/Carousel';
+
+const Luxurys = () => {
+  return (
+    <div className="Luxurys-section">
+      <div className="container ">
+        <div className="row">
+          <header className="App-header">
+            <h1>LUXURYS Details</h1>
+          </header>
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <div class="col mb-5">
+              <div class="card h-100 "  >
+                {/* <img src={luxury} className="card-img-top" /> */}
+                <Carousel>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={luxury}
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={luxury2}
+                      alt="Second slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={luxury3}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+                <div class="card-body">
+                  <h5 class="card-title text-black">
+                    <strong>LUXURY</strong>
+                  </h5>
+                  <p class="card-text text-black mb-3">
+                    Our project allows the user to filter out what exactly they
+                    want in their car or how it performs, which then filters
+                    through various databases from different car companies and
+                    compares the best options for them. For example, a user may
+                    own a construction company and needs a strong, sturdy truck
+                    that has great trunk space, and is capable of driving in
+                    heavy snow, rain, and even withstand hail. The user will
+                    instantly have access to many different trucks with the
+                    features and specs they need their truck to be able to have,
+                    as well as price range, mileage, capability, and horsepower.{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Luxurys;
---
+++ frontend/src/components/signup.js
@@ -0,0 +1,146 @@
+import * as React from "react";
+import {
+  Avatar,
+  Stack,
+  Typography,
+  TextField,
+  FormControl,
+} from "@mui/material";
+import { IconButton, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { app, db } from "./Firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { setDoc, doc } from "firebase/firestore";
+import img1 from './singup-login-pic/img1.png'
+
+
+export default function Sign_Up() {
+ 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  
+  const handleAction = () => {
+    const auth = getAuth();
+    console.log("done");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        // alert("register successfully")
+        
+        // navigate("/Home");
+
+        setDoc(doc(db, "users", res.user.uid), {
+          email: email,
+          password: password,
+          userId:res.user.uid
+        }).then(() => {
+          console.log("done2");
+        });
+      })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+        //   toast.error("Please check the Password");
+        // alert("Please check the Password")
+        }
+        if (error.code === "auth/user-not-found") {
+          // alert("Please check the Email");
+        }
+        });
+  };
+
+  return (
+    <Stack
+      sx={{
+        height: "100%",
+        border: "3px solid white",
+        borderRadius: "30px 2px",
+        pt: 6,
+      }}
+      direction="column"
+      alignItems="center"
+      alignContent="center"
+      justifyContent="center"
+      spacing={2}
+    >
+      
+      <Avatar
+        alt="account avetar"
+        src={img1}
+        sx={{ width: 100, height: 100 }}
+      >
+        {" "}
+      </Avatar>
+      
+      <Typography className="form-header"> SIGN UP </Typography>
+
+      <FormControl fullWidth sx={{ p: 2, pt: 0 }}>
+        <Stack direction="row" justifyContent="center" spacing={2}>
+          <IconButton>
+            <GoogleIcon
+              sx={{ width: 30, height: 30, color: "rgb(255, 255, 255)" }}
+            />
+          </IconButton>
+          <IconButton>
+            <AppleIcon
+              sx={{ width: 30, height: 30, color: "rgb(255, 255, 255)" }}
+            />
+          </IconButton>
+          <IconButton>
+            <FacebookIcon
+              sx={{ width: 30, height: 30, color: "rgb(255, 255, 255)" }}
+            />
+          </IconButton>
+        </Stack>
+
+        <TextField
+          type={"text"}
+          label="Full Name"
+          //   onChange={(e) => setName(e.target.value)}
+          variant="filled"
+          color="warning"
+          sx={{ width: "100%" }}
+        />
+
+        <TextField
+          type="email"
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          variant="filled"
+          color="warning"
+          sx={{ width: "100%", mt: 1 }}
+          
+        />
+
+        <TextField
+          label="Password"
+          variant="filled"
+          type="password"
+          sx={{ width: "100%", mt: 1 }}
+          color="warning"
+          //   type={values.showPassword ? "text" : "password"}
+          //   value={values.password}
+          onChange={(e) => setPassword(e.target.value)}
+          
+        />
+
+        <Button
+          variant="contained"
+          sx={{ mt: 5, backgroundColor: "white", color: "#223255" }}
+          onClick={handleAction}
+        >
+          sign up
+        </Button>
+
+        <Link to="/login" style={{ color: "white", mt: 2 }}>
+          Already have account,
+          <br /> Log in here
+        </Link>
+      </FormControl>
+    </Stack>
+  );
+}
